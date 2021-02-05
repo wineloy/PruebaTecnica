b0d3922cde8a4afd059fd444e3a8aea5d1904021
wineloy <wineloy@gmail.com>
2021-02-05 16:16:00 -0600

    inital commit

---
+++ Backend-Productos/Backend-Productos/Controllers/ProductosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Controlador encargado de la gestion de productos
+namespace Backend_Productos.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductosController : Controller
+    {
+        //Muestra la data de la base de datos
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Te he enviado una respuesta");
+        }
+
+        [Route("delete")]
+        [HttpDelete()]
+        public IActionResult delete()
+        {
+            return Ok("Te he enviado una respuesta desde Borrado");
+        }
+
+        [Route("edit")]
+        [HttpPut()]
+        public IActionResult Update()
+        {
+            return Ok("Te he enviado una respuesta desde Actualización");
+        }
+
+        [Route("create")]
+        [HttpPost()]
+        public IActionResult create()
+        {
+            return Ok("Te he enviado una respuesta desde creacion");
+        }
+
+    }
+}
